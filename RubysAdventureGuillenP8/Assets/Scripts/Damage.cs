35675a09034e05760a4a505b092c0adff98b93bc
Ellis Guillen <710619@student.dusd.net>
2023-12-07 15:26:19 -0800

    Modified Damage script

--- RubysAdventureGuillenP8/Assets/Scripts/Damage.cs
+++ RubysAdventureGuillenP8/Assets/Scripts/Damage.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
 }
